4d4fd66ecbf56f250ad25708ea6885fca4db3cc0
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-11 15:15:57 -0300

    Conteudo Estudos

--- frontend/js/estudos.js
+++ frontend/js/estudos.js
@@ -1,5 +1,8 @@
 window.onload = function() {
+    populaConteudos();
     // var proximoNivel = Math.pow((nivel + 1) * 4, 2)
+
+    // ===================Botoes Cabecalho======================
 
     document.getElementById('btnMeuHeroi').addEventListener('click', () => {
         console.log('meu heroi')
@@ -10,4 +13,36 @@ window.onload = function() {
     document.getElementById('verRanking').addEventListener('click', () => {
         console.log('Ver ranking')
     })
+
+    // ======================Buscar Conteudos=====================
+
+    async function pegarConteudos() {
+        try {
+            const resp = await fetch(`${host}/conteudo/get-all`, {
+                "method": "GET"
+            })
+            const data = await resp.json();
+            return(data);
+        } catch (error) {
+            return {"Erro": "Tivemos problemas ao se conectar com o servidor!"}
+        }
+    }
+
+    // ====================Popular Conteudos=========================
+    async function populaConteudos() {
+        document.getElementById('materiais').innerHTML += '';
+        carregamento();
+        const conteudos = await pegarConteudos();
+        pararCarregamento();
+        for(let x = 0; x < conteudos.length; x++) {
+            document.getElementById('materiais').innerHTML += `
+                <button id="${conteudos[x].idQuestionario}" onclick="irConteudo(this.id)" class="conteudoBotao">${conteudos[x].nome}</button>
+            `;
+        }
+    }
+}
+
+// ==================Ir para Conteudo=================
+function irConteudo(idQuestionario) {
+    console.log(idQuestionario)
 }
