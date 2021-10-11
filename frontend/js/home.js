957c2d5fe395f74e6856eeb17e8d223c9147f133
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-11 12:08:47 -0300

    Correcao de bug - carregamento

--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,5 +1,4 @@
 window.onload = function() {
-    carregamento()
     // =========Entrar====================
     document.querySelector('#homeEntrar').addEventListener('submit', async (event) => {
         event.preventDefault();
