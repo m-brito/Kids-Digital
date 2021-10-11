5a26487db13acc92b464411d4377611ac12bf6dc
Mauricio Brito <britomauricio2003@gmail.com>
2021-10-11 14:38:54 -0300

    Estudos - Cabecalho

--- frontend/js/estudos.js
+++ frontend/js/estudos.js
@@ -1,1 +1,13 @@
-// var proximoNivel = Math.pow((nivel + 1) * 4, 2)
+window.onload = function() {
+    // var proximoNivel = Math.pow((nivel + 1) * 4, 2)
+
+    document.getElementById('btnMeuHeroi').addEventListener('click', () => {
+        console.log('meu heroi')
+    })
+    document.getElementById('btnSair').addEventListener('click', () => {
+        console.log('Sair')
+    })
+    document.getElementById('verRanking').addEventListener('click', () => {
+        console.log('Ver ranking')
+    })
+}
